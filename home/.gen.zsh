4e8e2ee05f6d02522302149d578f4472b5f4b11b
Raphael <$EMAIL>
2014-04-17 04:32:52 -0300

    gen.zsh para gerar symlinks

---
+++ home/.gen.zsh
@@ -0,0 +1,6 @@
+# Faz um link simbolico do ~/.zprezto/runcoms/ para $HOME
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+      ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  done
