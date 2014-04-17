e7812ff145719384277441e34181fdab72c7c434
Raphael <$EMAIL>
2014-04-17 16:25:04 -0300

    add zsh files; rm .gen.zsh

--- home/.gen.zsh
+++
@@ -1,7 +0,0 @@
-# Faz um link simbolico do ~/.zprezto/runcoms/* p/ $HOME
-# source ~/.gen.zsh
-
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-      ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-  done
