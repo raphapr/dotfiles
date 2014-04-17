a2dd036e7d278c547d253a4e02dfbb3152c01681
Raphael <$EMAIL>
2014-04-17 04:45:01 -0300

    .gen.zsh comentários

--- home/.gen.zsh
+++ home/.gen.zsh
@@ -1,4 +1,5 @@
-# Faz um link simbolico do ~/.zprezto/runcoms/ para $HOME
+# Faz um link simbolico do ~/.zprezto/runcoms/* p/ $HOME
+# source ~/.gen.zsh
 
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
