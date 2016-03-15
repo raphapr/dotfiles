a1c60388b3be55b34dd38a54ecc4eb6068869d2b
Raphael P. Ribeiro <raphaelpr01@gmail.com>
2016-03-14 21:39:27 -0300

    changed tmux colors and update some files

--- home/.tmux/tmux_battery.sh
+++ home/.tmux/tmux_battery.sh
@@ -1,30 +1,20 @@
-#!/bin/bash - 
-#===============================================================================
-#
-#          FILE: .battery.sh
-# 
-#         USAGE: ./.battery.sh 
-# 
-#   DESCRIPTION: imprime coração preenchido + porcentagem da bateria se estiver carregado, se não, imprime coração vazio + porcentagem no tmux status bar
-# 
-#       OPTIONS: ---
-#  REQUIREMENTS: ---
-#          BUGS: ---
-#         NOTES: ---
-#        AUTHOR: Raphael P. Ribeiro 
-#  ORGANIZATION: 
-#       CREATED: 17-04-2014 17:39:36 BRT
-#      REVISION:  ---
-#===============================================================================
+#!/bin/bash
+# modified from http://ficate.com/blog/2012/10/15/battery-life-in-the-land-of-tmux/
 
-HEART_FULL="♥"
-HEART_EMPTY="♡"
-PORCENTAGEM=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | cut -d ":" -f 2 | tr -d ' ')
-ESTADO=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep state | cut -d ":" -f 2 | tr -d ' ')
+HEART='♥ '
 
+current_charge=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | cut -d ":" -f 2 | tr -d ' ' | tr -d '%')
+total_charge=100
 
-if [[ $ESTADO == "fully-charged" ]]; then 
-    echo $HEART_FULL $PORCENTAGEM
-else
-    echo $HEART_EMPTY $PORCENTAGEM
+charged_slots=$(echo "((($current_charge/$total_charge)*10)/3)+1" | bc -l | cut -d '.' -f 1)
+if [[ $charged_slots -gt 3 ]]; then
+  charged_slots=3
+fi
+
+echo -n '#[fg=colour208]'
+for i in `seq 1 $charged_slots`; do echo -n "$HEART"; done
+
+if [[ $charged_slots -lt 3 ]]; then
+  echo -n '#[fg=colour254]'
+  for i in `seq 1 $(echo "3-$charged_slots" | bc)`; do echo -n "$HEART"; done
 fi
