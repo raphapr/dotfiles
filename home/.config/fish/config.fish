3923a67979f0b01edb4eee7808e396c5a08340b6
Raphael P. Ribeiro <raphaelpr01@gmail.com>
2016-11-14 02:50:51 -0300

    update fish config

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -15,7 +15,12 @@ set BROWSER firefox
 set GPGKEY DBC876419930B2EB8447BFEFFA70B2729F47724C
 
 # fish vi mode
-fish_vi_key_bindings
+# ctrl+f only accept autosuggestion
+function fish_user_key_bindings
+    fish_vi_mode
+    bind -M insert \cf accept-autosuggestion
+    bind \cf accept-autosuggestion
+end
 
 # }}}
 # Aliases         ---------------------------------------------- {{{
