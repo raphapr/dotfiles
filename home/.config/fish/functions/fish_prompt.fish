f9e58a13eb7b2b75e0b0ca7e6a6313661821eb86
Raphael P. Ribeiro <raphaelpr01@gmail.com>
2018-06-30 02:45:28 -0300

    improve fish prompt with aws profile status

--- home/.config/fish/functions/fish_prompt.fish
+++ home/.config/fish/functions/fish_prompt.fish
@@ -62,14 +62,16 @@ function fish_prompt --description 'Write out the prompt'
         set prompt_status ' ' (set_color $fish_color_status) "[$last_status]" "$normal"
     end
 
-    echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) (prompt_hostname) $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_vcs_prompt) $normal $prompt_status "> "
+    echo -n -s (set_color $fish_color_user) "$USER" $normal ':' (set_color $color_cwd) (prompt_pwd) $normal (__fish_vcs_prompt) ' ' $normal $prompt_status
 
     if set -q VIRTUAL_ENV
-        echo -n -s (set_color brred) (basename "$VIRTUAL_ENV") (set_color normal) " "
+        echo -n -s (set_color brred) "v:"(basename "$VIRTUAL_ENV") (set_color normal) " "
     end
 
     if set -q AWS_PROFILE
-        echo -n -s (set_color cyan) (basename "aws:$AWS_PROFILE") (set_color normal) " "
+        echo -n -s (set_color cyan) "a:$AWS_PROFILE" (set_color normal) " "
     end
+
+    echo -n -s '> '
 
 end
