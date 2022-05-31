a712743a7e4a8ea5136a5aa5e8cf17e5df0a99f6
Raphael P. Ribeiro <raphaelpr01@gmail.com>
2022-05-31 18:31:07 -0300

    housekeeping

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -11,7 +11,7 @@ export TERM=screen-256color
 export EDITOR="nvim"
 export VIRTUALENV_PYTHON=/usr/bin/python2.7
 export GOPATH="$HOME/go"
-export BROWSER=google-chrome-stable
+export BROWSER=firefox
 export GPGKEY DBC876419930B2EB8447BFEFFA70B2729F47724C
 export FZF_DEFAULT_OPTS="--height 50%"
 export GEMDIR=(ruby -e 'print Gem.user_dir')
