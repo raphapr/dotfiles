4b9ad95892fb2c6ec9517b97979f4b67ade33eb3
Raphael Ribeiro <raphaelpr01@gmail.com>
2023-04-29 13:07:01 -0300

    nvim: fix fugitive maps

--- home/.config/nvim/lua/raphapr/plugins/fugitive.lua
+++ home/.config/nvim/lua/raphapr/plugins/fugitive.lua
@@ -1,6 +1,4 @@
 vim.keymap.set("n", "<leader>tt", vim.cmd.Git, { desc = "git: summary window" })
 vim.keymap.set("n", "<leader>tl", ":Gclog<CR>", { silent = true, desc = "git: log" })
-vim.keymap.set("n", "<leader>tp", ":Git push ", { desc = "git: push with args" })
-vim.keymap.set("n", "<leader>tP", function()
-  vim.cmd.Git({ "pull", "--rebase" })
-end, { desc = "git: pull rebase" })
+vim.keymap.set("n", "<leader>tp", ":Git push ", { desc = "git: push" })
+vim.keymap.set("n", "<leader>td", vim.cmd.Gvdiffsplit, { desc = "git: diff split vertically " })
