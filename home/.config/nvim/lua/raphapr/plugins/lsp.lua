caf675de533e1542fa97ce50f12b05d97c13e614
Raphael Ribeiro <raphaelpr01@gmail.com>
2023-05-19 19:20:21 -0300

    nvim: fix shell formatting and add Dockerfile formatting

--- home/.config/nvim/lua/raphapr/plugins/lsp.lua
+++ home/.config/nvim/lua/raphapr/plugins/lsp.lua
@@ -120,17 +120,19 @@ local nls = require("null-ls")
 local U = require("raphapr.plugins.utils")
 
 local fmt = nls.builtins.formatting
+local diag = nls.builtins.diagnostics
 
 -- Configuring null-ls
 nls.setup({
   sources = {
     fmt.black.with({ extra_args = { "--fast" } }),
     fmt.stylua,
-    fmt.shfmt,
+    fmt.beautysh,
     fmt.gofumpt,
     fmt.prettier.with({
       filetypes = { "html", "css", "yaml", "markdown", "json" },
     }),
+    diag.hadolint,
   },
   on_attach = function(client, bufnr)
     U.fmt_on_save(client, bufnr)
