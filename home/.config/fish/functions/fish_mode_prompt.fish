157c424f2af5f9425c61b9e73c30dd59d6e50768
Raphael Ribeiro <raphaelpr01@gmail.com>
2023-05-24 00:20:19 -0300

    nvim: more hydro prompt tweaks

--- home/.config/fish/functions/fish_mode_prompt.fish
+++ home/.config/fish/functions/fish_mode_prompt.fish
@@ -4,16 +4,17 @@ function fish_mode_prompt
         set --local vi_mode_symbol
         switch $fish_bind_mode
             case default
-                set vi_mode_color (set_color $fish_color_selection)
+                set vi_mode_color (set_color --bold black --background=89b4fa)
                 set vi_mode_symbol N
             case insert
-                set vi_mode_color (set_color $fish_color_selection)
+                set vi_mode_color (set_color --bold black --background a6e3a1 )
                 set vi_mode_symbol I
             case replace replace_one
                 set vi_mode_color (set_color $fish_color_match)
+                set vi_mode_color (set_color --bold black --background fab387)
                 set vi_mode_symbol R
             case visual
-                set vi_mode_color (set_color $fish_color_match)
+                set vi_mode_color (set_color --bold black --background f5c2e7)
                 set vi_mode_symbol V
         end
         echo -e "$vi_mode_color $vi_mode_symbol \x1b[0m "
--- home/.config/fish/functions/fish_prompt.fish
+++ home/.config/fish/functions/fish_prompt.fish
@@ -8,5 +8,5 @@ function fish_prompt --description Hydro
 
     set -l _extra_status "$_aws_profile"
 
-    echo -e "$_hydro_color_pwd$_hydro_pwd$hydro_color_normal $_hydro_color_git$$_hydro_git$hydro_color_normal$_hydro_color_duration$_hydro_cmd_duration$hydro_color_normal$_extra_status$_hydro_status$hydro_color_normal "
+    echo -e "$_hydro_color_pwd$_hydro_pwd$hydro_color_normal $_hydro_color_git$$_hydro_git$_extra_status$hydro_color_normal$_hydro_color_duration$_hydro_cmd_duration$hydro_color_normal$_hydro_status$hydro_color_normal "
 end
