44bb6b33614a4433735561e49208b11b56b06752
Raphael Ribeiro <raphaelpr01@gmail.com>
2023-07-30 09:32:38 -0300

    fish: add virtualfish to prompt

--- home/.config/fish/functions/fish_prompt.fish
+++ home/.config/fish/functions/fish_prompt.fish
@@ -6,7 +6,12 @@ function fish_prompt --description Hydro
         set _aws_profile (set_color f9e2af)"  $AWS_PROFILE "(set_color normal)
     end
 
-    set -l _extra_status "$_aws_profile"
+    if set -q VIRTUAL_ENV
+        set _virtual_env (set_color 8080ff)" "(basename "$VIRTUAL_ENV ")(set_color normal)
+    end
+
+    set -l _extra_status "$_virtual_env$_aws_profile"
 
     echo -e "$_hydro_color_pwd$_hydro_pwd$hydro_color_normal $_hydro_color_git$$_hydro_git$_extra_status$hydro_color_normal$_hydro_color_duration$_hydro_cmd_duration$hydro_color_normal$_hydro_status$hydro_color_normal "
 end
+
