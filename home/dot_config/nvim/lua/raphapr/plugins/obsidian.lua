e134202ce13a88b97b9e2b007e6d4c80db0cb593
Raphael Ribeiro <raphaelpr01@gmail.com>
2023-11-27 10:00:17 -0300

    nvim: fix obsidian daily date format

--- home/dot_config/nvim/lua/raphapr/plugins/obsidian.lua
+++ home/dot_config/nvim/lua/raphapr/plugins/obsidian.lua
@@ -3,7 +3,7 @@ require("obsidian").setup({
   notes_subdir = "Zettels",
   daily_notes = {
     folder = "Daily",
-    date_format = "%d/%m/%Y",
+    date_format = "%d-%m-%Y",
     template = "_Templates/daily.md",
   },
   templates = {
@@ -30,3 +30,4 @@ vim.keymap.set("n", "<leader>on", ":ObsidianNew<CR>", { desc = "Obsidian New Not
 vim.keymap.set("n", "<leader>ot", ":ObsidianTemplate<CR>", { desc = "Obsidian Template" })
 vim.keymap.set("n", "<leader>oq", ":ObsidianQuickSwitch<CR>", { desc = "Obsidian Quick Switcher" })
 vim.keymap.set("n", "<leader>os", ":ObsidianSearch<CR>", { desc = "Obsidian Search" })
+vim.keymap.set("n", "<leader>od", ":ObsidianToday<CR>", { desc = "Obsidian Daily" })
