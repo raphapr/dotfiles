058fe647bbf1bacfa54c598f33af4c3c3e0ab8c1
Raphael Ribeiro <raphaelpr01@gmail.com>
2024-01-22 17:58:16 -0300

    tmux: enable true color support

--- home/dot_config/nvim/lua/raphapr/plugins/misc.lua
+++ home/dot_config/nvim/lua/raphapr/plugins/misc.lua
@@ -31,8 +31,11 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
+    opts = {
+      flavour = "mocha",
+    },
     config = function()
-      vim.cmd.colorscheme("catppuccin-mocha")
+      vim.cmd.colorscheme("catppuccin")
     end,
   },
   -------------------------------------- bufdelete.nvim ---------------------------------
