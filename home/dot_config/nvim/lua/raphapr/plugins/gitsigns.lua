9d8fef23785e922c6e3cfe807d59d6f98d76290c
Raphael Ribeiro <raphaelpr01@gmail.com>
2024-01-30 23:32:47 -0300

    nvim: fix gitsigns setup

--- home/dot_config/nvim/lua/raphapr/plugins/gitsigns.lua
+++ home/dot_config/nvim/lua/raphapr/plugins/gitsigns.lua
@@ -4,9 +4,19 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {
       signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-      numhl = false,     -- Toggle with `:Gitsigns toggle_numhl`
-      linehl = false,    -- Toggle with `:Gitsigns toggle_linehl`
+      numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+      linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
       word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
     },
+    config = function()
+      require("gitsigns").setup()
+      vim.keymap.set("n", "<leader>tp", ":Gitsigns preview_hunk<CR>", { desc = "git: diff preview" })
+      vim.keymap.set(
+        "n",
+        "<leader>tl",
+        ":Gitsigns toggle_current_line_blame<CR>",
+        { desc = "git: toggle current line blame" }
+      )
+    end,
   },
 }
