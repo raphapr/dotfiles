f584bc7107972b664d462894b377090e85bbc7ab
Raphael Ribeiro <raphaelpr01@gmail.com>
2024-03-18 11:34:12 -0300

    nvim: add gitlinker and unity git config into a single file

---
+++ home/dot_config/nvim/lua/raphapr/plugins/git.lua
@@ -0,0 +1,67 @@
+return {
+  -------------------------------------- fugitive ---------------------------------------
+  {
+    "tpope/vim-fugitive",
+    event = { "VeryLazy" },
+    cmd = { "G", "Git", "Gclog", "Gvdiffsplit" },
+    keys = {
+      { "<leader>tb", ":Git blame<CR>", silent = true, desc = "git: blame" },
+    },
+  },
+  -------------------------------------- gitsigns ---------------------------------------
+  {
+    "lewis6991/gitsigns.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+      numhl = false,     -- Toggle with `:Gitsigns toggle_numhl`
+      linehl = false,    -- Toggle with `:Gitsigns toggle_linehl`
+      word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+    },
+    config = function()
+      require("gitsigns").setup()
+      vim.keymap.set("n", "<leader>tp", ":Gitsigns preview_hunk<CR>", { desc = "git: diff preview" })
+      vim.keymap.set(
+        "n",
+        "<leader>tl",
+        ":Gitsigns toggle_current_line_blame<CR>",
+        { desc = "git: toggle current line blame" }
+      )
+    end,
+  },
+  -------------------------------------- lazygit ----------------------------------------
+  {
+    "kdheepak/lazygit.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    event = { "VeryLazy" },
+    cmd = { "LazyGit" },
+    keys = {
+      { "<leader>tt", ":LazyGit<CR>", silent = true, desc = "git: call lazygit" },
+    },
+  },
+  -------------------------------------- gitlinker --------------------------------------
+  {
+    "ruifm/gitlinker.nvim",
+    dependencies = "nvim-lua/plenary.nvim",
+    event = "VeryLazy",
+    opts = {
+      mapping = nil,
+    },
+    config = function()
+      require("gitlinker").setup()
+      vim.api.nvim_set_keymap(
+        "n",
+        "<leader>ty",
+        '<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
+        { silent = true, desc = "git: open permalink in browser" }
+      )
+
+      vim.api.nvim_set_keymap(
+        "v",
+        "<leader>ty",
+        '<cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
+        { desc = "git: open permalink in browser" }
+      )
+    end,
+  },
+}
--- home/dot_config/nvim/lua/raphapr/plugins/lazygit.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "kdheepak/lazygit.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  event = { "VeryLazy" },
-  cmd = { "LazyGit" },
-  keys = {
-    { "<leader>tt", ":LazyGit<CR>", silent = true, desc = "git: call lazygit" },
-  },
-}
