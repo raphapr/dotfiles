624ac8ae8829148484b5a3245b45e68e839dbc68
Raphael Ribeiro <raphaelpr01@gmail.com>
2025-01-07 19:53:08 -0300

    nvim: add commands for toggling conform auto format

--- home/dot_config/nvim/lua/raphapr/plugins/conform.lua
+++ home/dot_config/nvim/lua/raphapr/plugins/conform.lua
@@ -1,8 +1,26 @@
 return {
   "stevearc/conform.nvim",
   event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
+  cmd = { "ConformInfo", "FormatDisable", "FormatEnable" },
   config = function()
+    vim.api.nvim_create_user_command("FormatDisable", function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = "Disable autoformat-on-save",
+      bang = true,
+    })
+    vim.api.nvim_create_user_command("FormatEnable", function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = "Re-enable autoformat-on-save",
+    })
+
     require("conform").setup({
       lsp_fallback = true,
       formatters_by_ft = {
@@ -23,10 +41,13 @@ return {
           prepend_args = { "--column-width", "150" },
         },
       },
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 500, lsp_format = "fallback" }
+      end,
     })
   end,
 }
