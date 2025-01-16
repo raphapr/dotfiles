5946d94352d725e1fc7df82262a3a46e325e83c0
Raphael Ribeiro <raphaelpr01@gmail.com>
2025-01-16 09:57:11 -0300

    nvim: configure neorg journal

--- home/dot_config/nvim/lua/raphapr/plugins/neorg.lua
+++ home/dot_config/nvim/lua/raphapr/plugins/neorg.lua
@@ -6,11 +6,14 @@ return {
     ft = { "norg" },
     cmd = "Neorg",
     build = ":Neorg sync-parsers",
+    dependencies = { { "pysan3/neorg-templates", dependencies = { "L3MON4D3/LuaSnip" } } },
     config = function()
       require("neorg").setup({
         load = {
           ["core.defaults"] = {},
           ["core.concealer"] = {},
+          ["external.templates"] = {},
+          ["core.summary"] = { config = { strategy = "by_path" } },
           ["core.dirman"] = {
             config = {
               workspaces = {
@@ -21,7 +24,9 @@ return {
           },
         },
       })
-      vim.keymap.set("n", "<leader>n", ":Neorg workspace notes<CR>", { desc = "Neorg: Open workspace notes" })
+      vim.keymap.set("n", "<leader>nn", ":Neorg workspace notes<CR>", { desc = "Neorg: Open workspace notes" })
+      vim.keymap.set("n", "<leader>nj", ":Neorg journal today<CR>", { desc = "Neorg: Open journal today" })
+      vim.keymap.set("n", "<leader>nt", ":Neorg templates load journal<CR>", { desc = "Neorg: Load journal template" })
       vim.api.nvim_create_autocmd("Filetype", {
         pattern = "norg",
         callback = function()
