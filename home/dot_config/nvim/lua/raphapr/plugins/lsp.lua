8b3f4e062f16ef5618cca73d2aa861506be1f8e2
Raphael Ribeiro <raphaelpr01@gmail.com>
2025-01-21 19:15:17 -0300

    nvim: reconfigure lsp without lsp-zero

--- home/dot_config/nvim/lua/raphapr/plugins/lsp.lua
+++ home/dot_config/nvim/lua/raphapr/plugins/lsp.lua
@@ -13,8 +13,6 @@ return {
         },
         hint_prefix = "󰏚  ",
       })
-      -- Reserve space for diagnostic icons
-      vim.opt.signcolumn = "yes"
     end,
   },
   ------------------------------------------------------
@@ -31,20 +29,12 @@ return {
     },
   },
   ------------------------------------------------------
-  -- lsp-zero
+  -- Autocompletion
   ------------------------------------------------------
   {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v3.x",
-    event = { "BufReadPre", "BufNewFile" },
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
     dependencies = {
-      -- LSP Support
-      { "neovim/nvim-lspconfig" },
-      { "williamboman/mason.nvim" },
-      { "williamboman/mason-lspconfig.nvim" },
-
-      -- Autocompletion
-      { "hrsh7th/nvim-cmp" },
       { "hrsh7th/cmp-buffer" },
       { "hrsh7th/cmp-path" },
       { "hrsh7th/cmp-nvim-lsp" },
@@ -54,67 +44,126 @@ return {
       { "petertriho/cmp-git" },
       { "onsails/lspkind-nvim" },
       { "saadparwaiz1/cmp_luasnip" },
+    },
+    config = function()
+      local cmp = require("cmp")
+      local lspkind = require("lspkind")
+
+      require("luasnip.loaders.from_vscode").lazy_load()
+
+      cmp.setup({
+        preselect = "item",
+        completion = {
+          completeopt = "menu,menuone,noinsert",
+        },
+        window = {
+          documentation = cmp.config.window.bordered(),
+        },
+        sources = {
+          { name = "path" },
+          { name = "nvim_lsp" },
+          { name = "nvim_lua" },
+          { name = "fish" },
+          { name = "git" },
+          { name = "render-markdown" },
+          { name = "buffer", keyword_length = 3 },
+          { name = "luasnip", keyword_length = 2 },
+          { name = "cmp_yanky" },
+          { name = "lazydev", group_index = 0 },
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-f>"] = cmp.mapping.scroll_docs(5),
+          ["<C-u>"] = cmp.mapping.scroll_docs(-5),
+          ["<C-k>"] = cmp.mapping.select_prev_item(),
+          ["<C-j>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = "select" }),
+          -- Simple tab complete
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            local col = vim.fn.col(".") - 1
+            if cmp.visible() then
+              cmp.select_next_item({ behavior = "select" })
+            elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
+              fallback()
+            else
+              cmp.complete()
+            end
+          end, { "i", "s" }),
+        }),
+        formatting = {
+          expandable_indicator = true,
+          fields = { "abbr", "kind", "menu" },
+          format = lspkind.cmp_format({
+            mode = "symbol",
+            maxwidth = 50,
+            menu = {
+              nvim_lsp = "[lsp]",
+              nvim_lua = "[lua-api]",
+              path = "[path]",
+              luasnip = "[snip]",
+              buffer = "[buf]",
+              gh_issues = "[issue]",
+              cmp_yanky = "[yanky]",
+              render_markdown = "[markdown]",
+              fish = "[fish]",
+            },
+          }),
+        },
+      })
+      require("cmp_git").setup({})
+    end,
+  },
+  ------------------------------------------------------
+  -- LSP
+  ------------------------------------------------------
+  {
+    "neovim/nvim-lspconfig",
+    cmd = "LspInfo",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "williamboman/mason.nvim" },
+      { "williamboman/mason-lspconfig.nvim" },
       { "L3MON4D3/LuaSnip", run = "make install_jsregexp" },
       { "rafamadriz/friendly-snippets" },
     },
+    init = function()
+      -- Reserve a space in the gutter
+      -- This will avoid an annoying layout shift in the screen
+      vim.opt.signcolumn = "yes"
+    end,
     config = function()
-      ------------------------------------------------------
-      -- lsp-zero
-      ------------------------------------------------------
-      local lsp = require("lsp-zero")
+      local lsp_defaults = require("lspconfig").util.default_config
 
-      lsp.on_attach(function(_, bufnr)
-        local function opts(desc)
-          return { desc = "LSP: " .. desc, buffer = bufnr, remap = false, silent = true }
-        end
-        local U = require("raphapr.utils")
+      -- Add cmp_nvim_lsp capabilities settings to lspconfig
+      -- This should be executed before you configure any language server
+      lsp_defaults.capabilities = vim.tbl_deep_extend("force", lsp_defaults.capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-        vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true })
-
-        vim.keymap.set("n", "<leader>li", vim.cmd.LspInfo, opts("Info"))
-
-        vim.keymap.set("n", "<leader>ls", ":LspStop<CR>:lua vim.notify('lsp stopped')<CR>", { silent = true, desc = "LSP: Stop" })
-
-        vim.keymap.set("n", "<leader>lt", ":LspStart<CR>:lua vim.notify('lsp started')<CR>", { silent = true, desc = "LSP: Start" })
-
-        vim.keymap.set("n", "<leader>le", ":LspRestart<CR>:lua vim.notify('lsp restart')<CR>", { silent = true, desc = "LSP: Restart" })
-
-        vim.keymap.set("n", "<leader>lf", function()
-          vim.diagnostic.open_float()
-        end, opts("Open float window"))
-        vim.keymap.set("n", "<leader>la", function()
-          vim.lsp.buf.code_action()
-        end, opts("Code action"))
-        vim.keymap.set("n", "<leader>lr", function()
-          vim.lsp.buf.rename()
-        end, opts("Rename"))
-        vim.keymap.set("n", "<leader>lc", function()
-          vim.diagnostic.reset()
-        end, opts("Clear diagnotics"))
-
-        vim.keymap.set("n", "K", function()
-          vim.lsp.buf.hover()
-        end, opts("Hover function"))
-        vim.keymap.set("n", "gd", function()
-          vim.lsp.buf.definition()
-        end, opts("go to definition"))
-        vim.keymap.set("n", "gs", U.go_to_definition_split, opts("Go to definition (split)"))
-        vim.keymap.set("n", "gr", function()
-          require("telescope.builtin").lsp_references()
-        end, opts("references"))
-        vim.keymap.set("n", "[[", function()
-          vim.diagnostic.goto_prev()
-        end, opts("Go to previous issue"))
-        vim.keymap.set("n", "]]", function()
-          vim.diagnostic.goto_next()
-        end, opts("Go to next issue"))
-      end)
-
-      lsp.set_sign_icons({
-        error = "✘",
-        warn = "▲",
-        hint = "⚑",
-        info = "",
+      -- LspAttach is where you enable features that only work
+      -- if there is a language server active in the file
+      vim.api.nvim_create_autocmd("LspAttach", {
+        desc = "LSP actions",
+        callback = function(event)
+          local U = require("raphapr.utils")
+          local function opts(desc)
+            return { desc = "LSP: " .. desc, buffer = event.buf, remap = false, silent = true }
+          end
+          vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true, buffer = event.buf })
+          vim.keymap.set("n", "<leader>li", vim.cmd.LspInfo, opts("Info"))
+          vim.keymap.set("n", "<leader>ls", ":LspStop<CR>:lua vim.notify('lsp stopped')<CR>", opts("Stop"))
+          vim.keymap.set("n", "<leader>lt", ":LspStart<CR>:lua vim.notify('lsp started')<CR>", opts("Start"))
+          vim.keymap.set("n", "<leader>le", ":LspRestart<CR>:lua vim.notify('lsp restart')<CR>", opts("Restart"))
+          vim.keymap.set("n", "<leader>lf", "<cmd>lua vim.diagnostic.open_float() <cr>", opts("Open float window"))
+          vim.keymap.set("n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action() <cr>", opts("Code action"))
+          vim.keymap.set("n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename() <cr>", opts("Rename"))
+          vim.keymap.set("n", "<leader>lc", "<cmd>lua vim.diagnostic.reset() <cr>", opts("Clear diagnotics"))
+          vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover() <cr>", opts("Hover function"))
+          vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition() <cr>", opts("go to definition"))
+          vim.keymap.set("n", "gs", U.go_to_definition_split, opts("Go to definition (split)"))
+          vim.keymap.set("n", "[[", "<cmd>lua vim.diagnostic.goto_prev()<cr>", opts("Go to previous issue"))
+          vim.keymap.set("n", "]]", "<cmd>lua vim.diagnostic.goto_next()<cr>", opts("Go to next issue"))
+          vim.keymap.set("n", "gr", "<cmd>lua require('telescope.builtin').lsp_references()<cr>", opts("references"))
+        end,
       })
 
       vim.lsp.set_log_level("off")
@@ -133,10 +182,14 @@ return {
           header = "",
           prefix = "",
         },
-      })
-
-      vim.diagnostic.config({
-        virtual_text = false,
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = "✘",
+            [vim.diagnostic.severity.WARN] = "▲",
+            [vim.diagnostic.severity.HINT] = "⚑",
+            [vim.diagnostic.severity.INFO] = "»",
+          },
+        },
       })
 
       ------------------------------------------------------
@@ -157,16 +210,15 @@ return {
           "yamlls",
         },
         handlers = {
-          lsp.default_setup,
+          -- default handler
+          -- applies to every language server without a custom handler
+          function(server_name)
+            require("lspconfig")[server_name].setup({})
+          end,
 
           ------------------------
           -- lsp servers settings
           ------------------------
-
-          function()
-            local lua_opts = lsp.nvim_lua_ls()
-            require("lspconfig").lua_ls.setup(lua_opts)
-          end,
 
           function()
             require("lspconfig").yamlls.setup({
@@ -208,80 +260,6 @@ return {
           end,
         },
       })
-
-      ------------------------------------------------------
-      -- cmp
-      ------------------------------------------------------
-
-      local lspkind = require("lspkind")
-      local cmp = require("cmp")
-      local cmp_action = lsp.cmp_action()
-
-      -- load friendly-snippets
-      require("luasnip.loaders.from_vscode").lazy_load()
-
-      cmp.setup({
-        preselect = "item",
-        completion = {
-          completeopt = "menu,menuone,noinsert",
-        },
-        window = {
-          documentation = cmp.config.window.bordered(),
-        },
-        sources = {
-          { name = "path" },
-          { name = "nvim_lsp" },
-          { name = "nvim_lua" },
-          { name = "fish" },
-          { name = "git" },
-          { name = "render-markdown" },
-          { name = "buffer", keyword_length = 3 },
-          { name = "luasnip", keyword_length = 2 },
-          { name = "cmp_yanky" },
-          { name = "lazydev", group_index = 0 },
-        },
-        mapping = cmp.mapping.preset.insert({
-          -- toggle completion menu
-          ["<C-e>"] = cmp_action.toggle_completion({}),
-
-          -- tab complete
-          ["<Tab>"] = cmp_action.tab_complete(),
-          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-
-          -- navigate between snippet placeholder
-          ["<C-d>"] = cmp_action.luasnip_jump_forward(),
-          ["<C-b>"] = cmp_action.luasnip_jump_backward(),
-
-          -- scroll documentation window
-          ["<C-f>"] = cmp.mapping.scroll_docs(5),
-          ["<C-u>"] = cmp.mapping.scroll_docs(-5),
-
-          ["<C-k>"] = cmp.mapping.select_prev_item(),
-          ["<C-j>"] = cmp.mapping.select_next_item(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        }),
-        formatting = {
-          format = lspkind.cmp_format({
-            mode = "symbol",
-            maxwidth = 50,
-            menu = {
-              nvim_lsp = "[lsp]",
-              nvim_lua = "[lua-api]",
-              path = "[path]",
-              luasnip = "[snip]",
-              buffer = "[buf]",
-              gh_issues = "[issue]",
-              cmp_yanky = "[yanky]",
-              render_markdown = "[markdown]",
-              fish = "[fish]",
-            },
-          }),
-        },
-      })
-
-      require("cmp_git").setup()
-
-      lsp.setup()
     end,
   },
 }
