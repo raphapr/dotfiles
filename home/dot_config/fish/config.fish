feeab1fade61837e733def9d2d4d5e4fbd00be92
Raphael Ribeiro <raphaelpr01@gmail.com>
2025-03-14 18:49:14 -0300

    fish: fix fzf integration

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -51,6 +51,8 @@ set NPM_PACKAGES "$HOME/.npm-packages"
 set PATH $PATH $NPM_PACKAGES/bin
 set MANPATH $NPM_PACKAGES/share/man $MANPATH
 
+fzf --fish | source
+
  #}}}
 # Bindings        ---------------------------------------------- {{{
 
