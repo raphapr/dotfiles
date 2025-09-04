232a60ef2cba38698a57658a5eab3b427d772650
Raphael Ribeiro <raphaelpr01@gmail.com>
2025-09-04 18:57:23 -0300

    nvim: update catpuccin integrations and make sure noto-fronts-emoji is installed

--- home/dot_config/nvim/lua/raphapr/plugins/colorscheme.lua
+++ home/dot_config/nvim/lua/raphapr/plugins/colorscheme.lua
@@ -5,7 +5,21 @@ return {
     priority = 1000,
     opts = {
       flavour = "mocha",
-      integrations = { overseer = true },
+      integrations = {
+        overseer = true,
+        codecompanion = true,
+        flash = true,
+        fzf = true,
+        gitsigns = true,
+        markdown = true,
+        cmp = true,
+        lsp_trouble = true,
+        telescope = { enabled = true },
+        mini = {
+          enabled = true,
+          indentscope_color = "", -- catppuccin color (eg. `lavender`) Default: text
+        },
+      },
     },
     config = function()
       vim.cmd.colorscheme("catppuccin")
--- home/dot_config/nvim/lua/raphapr/plugins/render-markdown.lua
+++ home/dot_config/nvim/lua/raphapr/plugins/render-markdown.lua
@@ -8,6 +8,7 @@ return {
       require("render-markdown").setup({
         file_types = { "markdown", "gitcommit" },
         html = { enabled = false },
+        latex = { enabled = false },
       })
       vim.keymap.set("n", "<leader>mt", ":lua require('render-markdown').toggle()<CR>", { noremap = true, desc = "Misc: Toggle render-markdown" })
     end,
