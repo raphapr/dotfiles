7e50d18fb5cd29aa298506e8331f0b1a626581f7
Raphael Ribeiro <raphaelpr01@gmail.com>
2025-09-05 18:40:29 -0300

    firefox: add user.js to the dotfiles and configure to sync with the default profile

--- home/run_onchange_after_04-create_symlinks.sh
+++ home/run_onchange_after_04-create_symlinks.sh
@@ -25,3 +25,21 @@ sudo chown root: /usr/lib/systemd/system-sleep/aftersleep
 
 sudo cp "${HOME}"/.config/udev/rules.d/*.rules /etc/udev/rules.d/
 sudo mkinitcpio -P
+echo "Udev rules copied and initramfs rebuilt"
+echo "Warning: no udev rules found"
+
+########################################################
+# firefox user.js
+########################################################
+
+if [ -d "${HOME}"/.mozilla/firefox ]; then
+  FIREFOX_PROFILE_DIR=$(find "${HOME}"/.mozilla/firefox -maxdepth 1 -type d -name "*.default-release" | head -n 1)
+  if [ -n "$FIREFOX_PROFILE_DIR" ]; then
+    ln -sf ~/.mozilla/firefox/user.js "$FIREFOX_PROFILE_DIR/user.js"
+    echo "Firefox user.js symlink created in $FIREFOX_PROFILE_DIR"
+  else
+    echo "No Firefox default-release profile found."
+  fi
+else
+  echo "Firefox directory not found."
+fi
