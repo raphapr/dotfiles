32475db898836ee4d4248e856a87293138250d00
Raphael Ribeiro <raphaelpr01@gmail.com>
2025-10-08 21:50:02 -0300

    rofi: change theme style

--- home/dot_config/rofi/launchers/type-1/executable_launcher.sh
+++ home/dot_config/rofi/launchers/type-1/executable_launcher.sh
@@ -12,9 +12,9 @@
 ## style-11    style-12    style-13    style-14    style-15
 
 dir="$HOME/.config/rofi/launchers/type-1"
-theme='style-1'
+theme='style-11'
 
 ## Run
 rofi \
-    -show drun \
-    -theme ${dir}/${theme}.rasi
+  -show drun \
+  -theme ${dir}/${theme}.rasi
