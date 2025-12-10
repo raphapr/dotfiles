471e14a693bba6ffc9e8fa670caa8be8577b41e3
Raphael Ribeiro <raphaelpr01@gmail.com>
2025-12-10 13:05:49 -0300

    nvim: configure strudel.nvim

--- home/dot_config/nvim/lua/raphapr/plugins/misc.lua
+++ home/dot_config/nvim/lua/raphapr/plugins/misc.lua
@@ -30,4 +30,20 @@ return {
       })
     end,
   },
+  -------------------------------------- strudel.nvim ----------------------------------
+  {
+    "gruvw/strudel.nvim",
+    build = "npm ci",
+    config = function()
+      require("strudel").setup()
+      local strudel = require("strudel")
+      vim.keymap.set("n", "<leader>sl", strudel.launch, { desc = "Launch Strudel" })
+      vim.keymap.set("n", "<leader>sq", strudel.quit, { desc = "Quit Strudel" })
+      vim.keymap.set("n", "<leader>st", strudel.toggle, { desc = "Strudel Toggle Play/Stop" })
+      vim.keymap.set("n", "<leader>su", strudel.update, { desc = "Strudel Update" })
+      vim.keymap.set("n", "<leader>ss", strudel.stop, { desc = "Strudel Stop Playback" })
+      vim.keymap.set("n", "<leader>sb", strudel.set_buffer, { desc = "Strudel set current buffer" })
+      vim.keymap.set("n", "<leader>sx", strudel.execute, { desc = "Strudel set current buffer and update" })
+    end,
+  },
 }
