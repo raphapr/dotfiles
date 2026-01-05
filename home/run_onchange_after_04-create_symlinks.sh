5df2640238ac1d3281830039214ea5553094b6d0
Raphael Ribeiro <raphaelpr01@gmail.com>
2026-01-05 12:51:42 -0300

    chezmoi: fix run script not finding the firefox default dir

--- home/run_onchange_after_04-create_symlinks.sh
+++ home/run_onchange_after_04-create_symlinks.sh
@@ -8,6 +8,10 @@ echo ">> Creating symlinks..."
 
 ln -sf ~/Dropbox ~/Cloud
 ln -sf ~/Cloud/Sync/krew ~/.krew
+
+if [ ! -d ~/.local/share/ptpython ]; then
+  mkdir -p ~/.local/share/ptpython
+fi
 ln -sf ~/Cloud/Sync/ptpython_history ~/.local/share/ptpython/history
 
 ########################################################
@@ -31,10 +35,15 @@ echo "Warning: no udev rules found"
 ########################################################
 
 if [ -d "${HOME}"/.mozilla/firefox ]; then
-  FIREFOX_PROFILE_DIR=$(find "${HOME}"/.mozilla/firefox -maxdepth 1 -type d -name "*.default-release" | head -n 1)
+  FIREFOX_PROFILE_DIR=$(find "${HOME}"/.mozilla/firefox -maxdepth 1 -name "*.default-release" | head -n 1)
   if [ -n "$FIREFOX_PROFILE_DIR" ]; then
     ln -sf ~/.mozilla/firefox/user.js "$FIREFOX_PROFILE_DIR/user.js"
     echo "Firefox user.js symlink created in $FIREFOX_PROFILE_DIR"
+    if [ ! -d "$FIREFOX_PROFILE_DIR/chrome" ]; then
+      mkdir "$FIREFOX_PROFILE_DIR/chrome"
+    fi
+    ln -sf ~/.mozilla/firefox/userChrome.css "$FIREFOX_PROFILE_DIR/chrome/userChrome.css"
+    echo "Firefox userChrome.css symlink created in $FIREFOX_PROFILE_DIR/chrome"
   else
     echo "No Firefox default-release profile found."
   fi
