9d0da7beb3c091c4029845dc405aa93a9fe70869
Raphael Ribeiro <raphaelpr01@gmail.com>
2026-03-27 15:09:51 -0300

    fix(nvim): use treesitter stable legacy branch

--- home/dot_config/nvim/lua/raphapr/plugins/treesitter.lua
+++ home/dot_config/nvim/lua/raphapr/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "master", -- v1.x (main) requires Neovim 0.12+; master is the stable legacy branch
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
     cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
