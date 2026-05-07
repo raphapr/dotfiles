2362d0560840bdbe174dc1d0df10d5f4fd077b36
Raphael Ribeiro <raphaelpr01@gmail.com>
2026-05-07 19:00:21 -0300

    chezmoi: enablle memreserver service to solve GPU VRAM issues on my destkop

--- home/run_onchange_after_99-enable-services.sh
+++ home/run_onchange_after_99-enable-services.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+HOSTNAME=$(hostname)
+
 echo ">> Enabling systemd services..."
 
 # enable systemd user services
 systemctl --user daemon-reload
 systemctl --user --now enable psd
 systemctl --user --now enable redshift
+ez
+if [[ "$HOSTNAME" == "moochacho" ]]; then
+  sudo systemctl enable memreserver.service
+fi
