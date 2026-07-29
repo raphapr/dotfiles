c4e12559627afee40177a7d9259b50380400ff43
Raphael Ribeiro <raphaelpr01@gmail.com>
2026-07-29 18:26:26 -0300

    yazi: switch bookmarks plugin to yamb

--- home/dot_config/yazi/init.lua
+++ home/dot_config/yazi/init.lua
@@ -1,17 +1,2 @@
 -- ~/.config/yazi/init.lua
-require("bookmarks"):setup({
-  last_directory = {
-    enable = true,
-    persist = true,
-    mode = "dir",
-  },
-  notify = {
-    enable = true,
-    timeout = 1,
-    message = {
-      new = "New bookmark '<key>' -> '<folder>'",
-      delete = "Deleted bookmark in '<key>'",
-      delete_all = "Deleted all bookmarks",
-    },
-  },
-})
+require("yamb"):setup({ jump_notify = true })
